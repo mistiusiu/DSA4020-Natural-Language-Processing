2a626aff55641f1b909b1f87c9f876e645a44967
Misati Nyambane <misatiquanta@gmail.com>
2026-08-02 23:04:44 +0300

    chore: Lazy model loading

--- app/translation/apps.py
+++ app/translation/apps.py
@@ -3,14 +3,3 @@ from django.apps import AppConfig
 
 class TranslationConfig(AppConfig):
     name = 'translation'
-
-
-    def ready(self):
-
-        from translation.classes import (
-            ModelRegistry
-        )
-
-        registry = ModelRegistry()
-
-        registry.load()
